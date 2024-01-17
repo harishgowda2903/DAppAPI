9be04411b0331c8debea0a522a17ff921a4865f1
Harish Gowda <har1996gowda@gmail.com>
2024-01-17 20:36:32 +0530

    Add project files.

---
+++ DAppAPI/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using DAppAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DAppAPI.Controllers
+{
+    [ApiController]
+    [Route("api/Users")]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DAppAPI/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using DAppAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAppAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
